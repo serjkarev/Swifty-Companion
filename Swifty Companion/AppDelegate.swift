ca90ad3319b57aa8fcd8328c594dc795bdc6c823
skarev <sevenkss95@gmail.com>
2019-08-09 23:24:43 +0300

    Add UI elements

--- Swifty Companion/AppDelegate.swift
+++ Swifty Companion/AppDelegate.swift
@@ -13,9 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    //start work on project
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
--- Swifty Companion/ViewController.swift
+++ Swifty Companion/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
 
+    }
 
 }
 
